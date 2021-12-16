9c70a0f74a37d288818288ed328c87ac429926cc
RavenousWolf526 <95888458+RavenousWolf526@users.noreply.github.com>
2021-12-16 17:19:21 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var w = [20,25,39,48]
+
+function setup() 
+{
+  createCanvas(400,400);
+var sum = w[0]+w[1]+w[2]+w[3]
+var avg = sum/w.length
+console.log (avg)
+}
+
+function draw() 
+{
+background(51);
+
+}
+
